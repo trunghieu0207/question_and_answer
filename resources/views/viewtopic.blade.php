778929aed0cd0c7ec93d11ec5167168535841822
thanhnhatnt <1612390@hcmut.edu.vn>
2019-07-26 13:29:09 +0700

    Update viewtopic.blade.php

--- resources/views/viewtopic.blade.php
+++ resources/views/viewtopic.blade.php
@@ -58,16 +58,25 @@
 			<h3>Answer</h3>
 		</div>
 		<div class="row px-3 pt-3">
-			<div class="col-sm-1"><img src="img/avatar/{{$question->user->avatar}}" class="test rounded-circle align-middle"></div>
+			<div class="col-sm-1">
+				<img src="img/avatar/{{$question->user->avatar}}" class="test rounded-circle align-middle">
+				<br>
+				<br>
+				<div class="d-flex" style="justify-content :center; align-items:center;  font-size:200%; color:#66ad1f">
+					<i class="fa fa-check" aria-hidden="true"></i>
+				</div>
+				
+			</div>
 			<div class="col-sm-11">
 				<div class="font-weight-bold" style="color:#787878; font-size: 20px">{{$answer->user->fullname}}   
-					<i class="float-right fa fa-pencil-square-o" aria-hidden="true" style="margin-right:10px; font-size:120%%"></i>				 
+					<i class="float-right fa fa-pencil-square-o" aria-hidden="true" style="margin-right:10px; font-size:120%"></i>				 
 				</div>	
 				<div>
 					<small class="text-muted" style="color:#5488c7;">
 						<i class="fa fa-calendar" aria-hidden="true"> </i> {{$answer->created_at}} 
 					</small>	
 				</div>
+				<br>
 				<p>{{$answer->content}}</p>
 				<div class="row" style=" color:#787878; font-size: 20px ; margin-bottom: 10px">
 					<div class="col-sm-1">
