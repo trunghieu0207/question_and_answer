5cc1a78cffe15d3529eb64b990490cf941375494
thanhnhatnt <1612390@hcmut.edu.vn>
2019-07-26 15:42:39 +0700

    Update viewtopic.blade.php

--- resources/views/viewtopic.blade.php
+++ resources/views/viewtopic.blade.php
@@ -6,9 +6,10 @@
 			<div class="col-sm-1"><img src="{{asset('img/avatar')}}/{{$question->user->avatar}}" class="test rounded-circle align-middle"></div>
 			<div class="col-sm-11">
 				<div class="font-weight-bold" style="color:#787878; font-size: 25px">{{$question->user->fullname}}
+					@if (Session::get('id')==$question->user_id)
 					<i class="float-right fa fa-trash" aria-hidden="true" style="margin-right:10px; font-size: 120%; "></i>   
 					<i class="float-right fa fa-pencil-square-o" aria-hidden="true" style="margin-right:10px; font-size:120%"></i>
-				  	
+				  	@endif
 				</div>	
 				<div>
 					<small class="text-muted" style="color:#5488c7;">
@@ -40,8 +41,8 @@
 			</div>			
 		</div>
 	</div>
-	
-    <div class="card shadow" style="margin-top: 20px; margin-bottom: 20px;" >
+	@if (Auth::check())
+    <div class="card shadow" style="margin-top: 20px;" >
 		
 		<div class="card-body">
 			<form>
@@ -52,13 +53,14 @@
 			</form>
 		</div>		
 	</div>
-
-	<div class="card shadow" style="margin-bottom: 20px; " >
+	@endif
+	<div class="card shadow" style="margin-top: 20px; margin-bottom: 20px; " >
 		<div class="card-header">
 			<h3>Answer</h3>
 		</div>
+		@foreach($answers as $answer)
 		<div class="row px-3 pt-3">
-			@foreach($answers as $answer)
+			
 			<div class="col-sm-1">
 				<img src="{{asset('img/avatar')}}/{{$answer->user->avatar}}" class="test rounded-circle align-middle">
 				<br>
@@ -71,9 +73,11 @@
 				
 			</div>
 			<div class="col-sm-11">
+				@if (Session::get('id')==$answer->user_id)
 				<div class="font-weight-bold" style="color:#787878; font-size: 20px">{{$answer->user->fullname}}   
 					<i class="float-right fa fa-pencil-square-o" aria-hidden="true" style="margin-right:10px; font-size:120%"></i>				 
-				</div>	
+				</div>
+				@endif	
 				<div>
 					<small class="text-muted" style="color:#5488c7;">
 						<i class="fa fa-calendar" aria-hidden="true"> </i> {{$answer->created_at}} 
@@ -90,13 +94,16 @@
 						<i class="fa fa-thumbs-down"></i>
 						{{$answer->total_dislike}}
 					</div>
+					@if (Session::get('id')==$question->user_id)
 					<div class="col-sm-10 d-flex justify-content-sm-end">
 						<a href="{{asset('bestanswer')}}/{{$answer->_id}}"><button type="button" class="float-right btn btn-success">Best Answer</button></a>
 					</div>
+					@endif
 				</div>	
-			</div>
-			@endforeach
-		</div>		
+			</div>			
+		</div>	
+		<hr>
+		@endforeach	
 	</div>
 
 </div>
