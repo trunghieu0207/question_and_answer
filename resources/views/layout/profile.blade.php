5e1e3ec114e6e0f95278a09ebada07baffe7cc13
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2019-07-29 13:52:20 +0700

    add signup and signin

--- resources/views/layout/profile.blade.php
+++ resources/views/layout/profile.blade.php
@@ -30,8 +30,8 @@
                         <button class="badge btn btn-warning" data-toggle="modal" data-target="#exampleModal">change
                             avatar</button>
                         <div class="nav flex-column nav-pills my-3 bg-white border">
-                            <button class="btn nav-link @if(!empty($active_personal_info)) active @endif">Personal infomation</button>
-                            <button class="btn nav-link @if(!empty($active_change_pass)) active @endif">Change password</button>
+                            <a href="{{ route('information') }}" class="btn nav-link @if(!empty($active_personal_info)) active @endif" >Personal information</a>
+                            <a href="{{ route('change-password') }}" class="btn nav-link @if(!empty($active_change_pass)) active @endif">Change password</a>
                             <a href="{{ route('manage_question') }}" class="btn nav-link @if(!empty($active_manage_question)) active @endif">Manage question</a>
                             <a href="{{ route('manage_answer') }}" class="btn nav-link @if(!empty($active_manage_answer)) active @endif">Manage answer</a>
                             <a href="{{ route('log-out') }}" class="btn nav-link">Sign out</a>
