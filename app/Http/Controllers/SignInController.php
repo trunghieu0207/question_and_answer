a67bf5f210476ee48737d749465ef77879341bbf
thanhnhatnt <1612390@hcmut.edu.vn>
2019-07-30 15:47:48 +0700

    Update clean signin

--- app/Http/Controllers/SignInController.php
+++ app/Http/Controllers/SignInController.php
@@ -4,55 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
-use \App\User;
-use Illuminate\Support\Facades\Schema;
-use \App\Chitiet;
-use DB;
-use Illuminate\Support\collection;
+use App\User;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests;
 use Validator;
-use App\Http\Requests\LoginRequest;
-use App\Notification;
+
 
 class SignInController extends Controller
 {
-    public function view(){
-        if(Auth::check()){
-    	   return redirect()->route('home-page');
-        } else {
-            return view('signin');
-        }
+    public function view()
+    {    
+        return view('signin');
     }
-    public function viewtest(){
+
+    public function viewtest()
+    {
         return view('test');
     }
 
-    public function postSignIn(Request $request) {
+    public function postSignIn(Request $request)
+    {
         $email = $request->email;
         $password = $request->password;
-        $user = User::where('email', '=', $email)->first();
-        if(Auth::attempt(['email'=>$email, 'password'=>$password]))
+        if(!Auth::attempt(['email'=>$email, 'password'=>$password]))
         {
-            Session()->put('id',$user->_id);
-            Session()->put('username',$user->fullname);
-            Session()->put('avatar','img\avatar\\'.$user->avatar);
-            $notifications = Notification::where('user_id','=',$user->_id)->get();
-            Session()->put('notifications',$notifications);
-            return redirect()->route('home-page');
+            return redirect()->back()->withInput($request->only('email', 'remember'))
+            ->withErrors(['approve' => 'The email or password is incorrect.',]);
         }
         else
         {
-            return redirect()->back()->withInput($request->only('email', 'remember'))->withErrors([
-                'approve' => 'The email or password is incorrect.',
-            ]);
-         //    Session()->flash('error','The email or password is incorrect');
-        	// return view('page.signin');
+            return redirect()->route('home-page');            
         }
     }
-    public function logout(){
+
+    public function logout()
+    {
         Auth::logout();
         Session()->flush();
+        
         return redirect()->route('home-page');
     }
 }
--- resources/views/viewtopic.blade.php
+++ resources/views/viewtopic.blade.php
@@ -28,20 +28,22 @@
 </script>
 @endsection
 @section('content')
+
+
 <div class="container mt-5">
+    <!-- Start Question Block -->
     <div class="card shadow">
         <div class="row px-3 pt-3">
-            <div class="col-sm-1"><img src="{{asset('img/avatar')}}/{{$question->user->avatar}}"
-                    class="test rounded-circle align-middle"></div>
+            <div class="col-sm-1"><img src="{{asset('img/avatar')}}/{{$question->user->avatar}}"class="test rounded-circle align-middle"></div>
+            
+            <!-- Start Username, Date, Edit, Delete Block -->
             <div class="col-sm-11">
                 <div class="font-weight-bold" style="color:#787878; font-size: 25px">{{$question->user->fullname}}
-
-                    <!-- <i class="float-right fa fa-trash" aria-hidden="true" style="margin-right:10px; font-size: 120%; "></i>    -->
                     <!-- Button HTML (to Trigger Modal) -->
-                    @if($question->user_id==Session::get('id'))
-                    <a href="#myModal" data-toggle="modal"><i class="float-right fa fa-trash" aria-hidden="true"
-                            style="margin-right:10px; font-size: 120%; "></i></a>
-
+                    @if(Auth::check())
+                    @if($question->user_id==Auth::user()->id)
+                    <a href="#myModal" data-toggle="modal">
+                        <i class="float-right fa fa-trash" aria-hidden="true"style="margin-right:10px; font-size: 120%; "></i></a>
                     <!-- Modal HTML -->
                     <div id="myModal" class="modal fade" tabindex="-1">
                         <div class="modal-dialog">
@@ -68,6 +70,7 @@
                             aria-hidden="true" style="margin-right:10px; font-size:120%"></i></a>
                     @else
                     @endif
+                    @endif
 
                 </div>
                 <div>
@@ -77,48 +80,54 @@
                 </div>
                 <br>
             </div>
+            <!-- End Username, Date, Edit, Delete Block -->
+            
+            <!-- Start Question Title Block -->
             <div class="col-sm-12 d-flex justify-content-sm-between">
                 <h3>{{$question->title}}</h3>
                 <span class="badge badge-info" style="height: 20px">{{$question->category->name}}</span>
             </div>
+            <!-- End Question Title Block -->
+
+            <!-- Start Question Content Block -->
             <div class="col-sm-12" style="margin-left: 10px">
-                <p>
-                    {!! $question->content !!}
-                </p>
+                <p>{!! $question->content !!}</p>
                 @if(!file_exists(public_path().'files/{{ $question->attachment_path }}'))
-                <h6>Attchments</h6><a target="blank" href="{{asset('files/'.$question->attachment_path)}}"><i>{{$question->attachment_path}}</i></a>
+                    <h6>Attachments</h6>
+                    <a target="blank" href="{{asset('files/'.$question->attachment_path)}}"><i>{{$question->attachment_path}}</i></a>
                 @else
                 @endif
                 <div class="row" style="width: 300px; color:#787878; font-size: 20px; margin-bottom: 10px">
                     <div class="col-sm">
                         @if (Auth::check())
-
-                        <a href="{{asset('like')}}/{{$question->_id}}/Question/{{Session::get('id')}}"><i
-                                class="fa fa-thumbs-up"></i></a> {{$question->total_like}}
+                            <a href="{{asset('like')}}/{{$question->_id}}/Question/{{Auth::user()->id}}">
+                            <i class="fa fa-thumbs-up"></i></a> {{$question->total_like}}
                         @else
-                        <a href="{{route('sign-in')}} " style="color:#787878"><i class="fa fa-thumbs-up"></i></a>
-                        {{$question->total_like}}
+                            <a href="{{route('sign-in')}} " style="color:#787878"><i class="fa fa-thumbs-up"></i></a>
+                            {{$question->total_like}}
                         @endif
                     </div>
                     <div class="col-sm">
                         @if (Auth::check())
-                        <a href="{{asset('dislike')}}/{{$question->_id}}/Question/{{Session::get('id')}}"><i
-                                class="fa fa-thumbs-down"></i></a>
-                        {{$question->total_dislike}}
+                            <a href="{{asset('dislike')}}/{{$question->_id}}/Question/{{Auth::user()->id}}">
+                            <i class="fa fa-thumbs-down"></i></a> {{$question->total_dislike}}
                         @else
-                        <a href="{{route('sign-in')}}" style="color:#787878"><i class="fa fa-thumbs-down"></i></a>
-                        {{$question->total_dislike}}
+                            <a href="{{route('sign-in')}}" style="color:#787878"><i class="fa fa-thumbs-down"></i></a>
+                            {{$question->total_dislike}}
                         @endif
-
                     </div>
                     <div class="col-sm">
-                        <i class="fa fa-reply"></i>
-                        {{$question->total_answer}}
+                        <i class="fa fa-reply"></i> {{$question->total_answer}}
                     </div>
                 </div>
             </div>
+            <!-- End Question Content Block -->
         </div>
     </div>
+    <!-- End Question Block -->
+
+
+    <!-- Start Insert Answer Block -->
     @if (Auth::check())
     <div class="card shadow" style="margin-top: 20px;">
         <div class="card-body">
@@ -132,7 +141,6 @@
                                 <input id="fuMain" name="attachment" type="file">
                             </div>
                         </div>
-
                         <div class="col-sm-8">
                             <textarea id="MyID" name="content"></textarea>
                         </div>
@@ -143,155 +151,138 @@
         </div>
     </div>
     @endif
+    <!-- End Insert Answer Block -->
 
+    <!-- Start Answer Block -->
     <div class="card shadow" style="margin-top: 20px; margin-bottom: 20px; ">
         <div class="card-header">
             <h3>Answer</h3>
         </div>
-        <!-- ---------------------------------------------------------------------------------------------------------------------------------------- -->
+
+        <!-- Start Best Answer Block -->        
         @if ($best_answer!=null)
         <div class="row px-3 pt-3">
-
             <div class="col-sm-1">
-                <img src="{{asset('img/avatar')}}/{{$best_answer->user->avatar}}"
-                    class="test rounded-circle align-middle">
+                <img src="{{asset('img/avatar')}}/{{$best_answer->user->avatar}}" class="test rounded-circle align-middle">
                 <br>
-                <br>
-
+                <br>                
                 <div class="d-flex" style="justify-content :center; align-items:center;  font-size:200%; color:#66ad1f">
                     <i class="fa fa-check" aria-hidden="true"></i>
                 </div>
-
-
             </div>
             <div class="col-sm-11">
                 <div class="font-weight-bold" style="color:#787878; font-size: 20px">{{$best_answer->user->fullname}}
-                    @if($best_answer->user_id==Session::get('id'))
-                    <a href="{{asset('editanswer')}}/{{ $best_answer->id }}"><i
-                            class="float-right fa fa-pencil-square-o" aria-hidden="true"
-                            style="margin-right:10px; font-size:120%"></i></a>@else
+                    @if($best_answer->user_id==Auth::user()->id)
+                    <a href="{{asset('editanswer')}}/{{ $best_answer->id }}">
+                    <i class="float-right fa fa-pencil-square-o" aria-hidden="true" style="margin-right:10px; font-size:120%"></i></a>
                     @endif
                 </div>
-
                 <div>
                     <small class="text-muted" style="color:#5488c7;">
                         <i class="fa fa-calendar" aria-hidden="true"> </i> {{$best_answer->created_at}}
                     </small>
                 </div>
                 <br>
-
-
-                <p>
-                    {!! $best_answer->content !!}
-                </p>
-                 @if(!file_exists(public_path().'files/{{ $best_answer->attachment_path }}'))
-                 <h6>Attchments</h6><a target="blank" href="{{asset('files/'.$best_answer->attachment_path)}}"><i>{{$best_answer->attachment_path}}</i></a>
-                @else
+                <p>{!! $best_answer->content !!}</p>
+                @if(!file_exists(public_path().'files/{{ $best_answer->attachment_path }}'))
+                    <h6>Attachments</h6>
+                    <a target="blank" href="{{asset('files/'.$best_answer->attachment_path)}}"><i>{{$best_answer->attachment_path}}</i></a>
                 @endif
                 <div class="row" style=" color:#787878; font-size: 20px ; margin-bottom: 10px">
                     <div class="col-sm-1">
                         @if(Auth::check())
-
-                        <a href="{{asset('like')}}/{{$best_answer->_id}}/Answer/{{Session::get('id')}}"><i
-                                class="fa fa-thumbs-up"></i></a>
-                        {{$best_answer->total_like}}
+                            <a href="{{asset('like')}}/{{$best_answer->_id}}/Answer/{{Auth::user()->id}}">
+                            <i class="fa fa-thumbs-up"></i></a> {{$best_answer->total_like}}
                         @else
-                        <a href="{{route('sign-in')}}"><i class="fa fa-thumbs-up" style="color:#787878"></i></a>
-                        {{$best_answer->total_like}}
+                            <a href="{{route('sign-in')}}"><i class="fa fa-thumbs-up" style="color:#787878"></i></a>
+                            {{$best_answer->total_like}}
                         @endif
                     </div>
                     <div class="col-sm-1">
                         @if(Auth::check())
-                        <a href="{{asset('dislike')}}/{{$best_answer->_id}}/Answer/{{Session::get('id')}}"><i
-                                class="fa fa-thumbs-down"></i></a>
-                        {{$best_answer->total_dislike}}
+                            <a href="{{asset('dislike')}}/{{$best_answer->_id}}/Answer/{{Auth::user()->id}}">
+                            <i class="fa fa-thumbs-down"></i></a> {{$best_answer->total_dislike}}
                         @else
-                        <a href="{{route('sign-in')}}"><i class="fa fa-thumbs-down" style="color:#787878"></i></a>
-                        {{$best_answer->total_dislike}}
+                            <a href="{{route('sign-in')}}"><i class="fa fa-thumbs-down" style="color:#787878"></i></a>
+                            {{$best_answer->total_dislike}}
                         @endif
                     </div>
-                    @if (Session::get('id')==$question->user_id)
+                    @if (Auth::user()->id==$question->user_id)
                     <div class="col-sm-10 d-flex justify-content-sm-end">
                         <a href="{{asset('removebestanswer')}}/{{$best_answer->_id}}"><button type="button"
                                 class="float-right btn btn-warning">Remove Best Answer</button></a>
                     </div>
                     @else
                     @endif
-
                 </div>
             </div>
         </div>
         <hr>
         @endif
+        <!-- End Best Answer Block --> 
 
-
-
-        <!-- //------------------------------------------------------------------------------------------------------------------- -->
+        <!-- Start Other Answers Block -->
         @foreach($answers as $answer)
-        @if (($best_answer==null) or (($best_answer!=null)and($answer->_id!=$best_answer->_id)))
-
+        @if (($best_answer==null) or (($best_answer!=null) and ($answer->_id!=$best_answer->_id)))
         <div class="row px-3 pt-3">
-
             <div class="col-sm-1">
                 <img src="{{asset('img/avatar')}}/{{$answer->user->avatar}}" class="test rounded-circle align-middle">
                 <br>
                 <br>
                 @if ($question->best_answer_id == $answer->_id)
-                <div class="d-flex" style="justify-content :center; align-items:center;  font-size:200%; color:#66ad1f">
-                    <i class="fa fa-check" aria-hidden="true"></i>
-                </div>
+                    <div class="d-flex" style="justify-content :center; align-items:center;  font-size:200%; color:#66ad1f">
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                    </div>
                 @endif
-
             </div>
             <div class="col-sm-11">
-
                 <div class="font-weight-bold" style="color:#787878; font-size: 20px">{{$answer->user->fullname}}
-                    @if (Session::get('id')==$answer->user_id)
-                    <a href="{{asset('editanswer')}}/{{ $answer->id }}"><i class="float-right fa fa-pencil-square-o"
-                            aria-hidden="true" style="margin-right:10px; font-size:120%"></i> </a>
+                    @if (Auth::check())
+                    @if (Auth::user()->id==$answer->user_id)
+                        <a href="{{asset('editanswer')}}/{{ $answer->id }}"><i class="float-right fa fa-pencil-square-o"
+                                aria-hidden="true" style="margin-right:10px; font-size:120%"></i> </a>
+                    @endif
                     @endif
                 </div>
-
                 <div>
                     <small class="text-muted" style="color:#5488c7;">
                         <i class="fa fa-calendar" aria-hidden="true"> </i> {{$answer->created_at}}
                     </small>
                 </div>
                 <br>
-
-                <p>
-                    {!! $answer->content !!}
-                </p>
+                <p>{!! $answer->content !!}</p>
                 @if(!file_exists(public_path().'files/{{ $answer->attachment_path }}'))
-                 <h6>Attchments</h6><a target="blank" href="{{asset('files/'.$answer->attachment_path)}}"><i>{{$answer->attachment_path}}</i></a>
+                    <h6>Attachments</h6>
+                    <a target="blank" href="{{asset('files/'.$answer->attachment_path)}}"><i>{{$answer->attachment_path}}</i></a>
                 @else
                 @endif
                 <div class="row" style=" color:#787878; font-size: 20px ; margin-bottom: 10px">
                     <div class="col-sm-1">
                         @if(Auth::check())
-                        <a href="{{asset('like')}}/{{$answer->_id}}/Answer/{{Session::get('id')}}"><i
-                                class="fa fa-thumbs-up"></i></a>
-                        {{$answer->total_like}}
+                            <a href="{{asset('like')}}/{{$answer->_id}}/Answer/{{Auth::user()->id}}">
+                            <i class="fa fa-thumbs-up"></i></a> {{$answer->total_like}}
                         @else
-                        <a href="{{ route('sign-in') }} " style="color:#787878"><i class="fa fa-thumbs-up"></i></a>
-                        {{$answer->total_like}}
+                            <a href="{{ route('sign-in') }} " style="color:#787878"><i class="fa fa-thumbs-up"></i></a>
+                            {{$answer->total_like}}
                         @endif
                     </div>
                     <div class="col-sm-1">
                         @if(Auth::check())
-                        <a href="{{asset('dislike')}}/{{$answer->_id}}/Answer/{{Session::get('id')}}"><i
-                                class="fa fa-thumbs-down"></i></a>
-                        {{$answer->total_dislike}}
+                            <a href="{{asset('dislike')}}/{{$answer->_id}}/Answer/{{Auth::user()->id}}">
+                            <i class="fa fa-thumbs-down"></i></a>
+                            {{$answer->total_dislike}}
                         @else
-                        <a href="{{ route('sign-in') }}" style="color:#787878"><i class="fa fa-thumbs-down"></i></a>
-                        {{$answer->total_dislike}}
+                            <a href="{{ route('sign-in') }}" style="color:#787878"><i class="fa fa-thumbs-down"></i></a>
+                            {{$answer->total_dislike}}
                         @endif
                     </div>
-                    @if (Session::get('id')==$question->user_id)
+                    @if (Auth::check())
+                    @if (Auth::user()->id==$question->user_id)
                     <div class="col-sm-10 d-flex justify-content-sm-end">
                         <a href="{{asset('bestanswer')}}/{{$answer->_id}}"><button type="button"
                                 class="float-right btn btn-success">Best Answer</button></a>
                     </div>
+                    @endif
                     @endif
                 </div>
             </div>
